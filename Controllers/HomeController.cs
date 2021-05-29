271b06a3a8ffc92f49cfffefc524e90825d0cc54
Sreehari <sreehari.s@science.christuniversity.in>
2021-05-29 19:54:56 +0530

    Search By Location Feature added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,15 @@
 ﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
 using RestSharp;
 using System;
+using System.CodeDom;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 using VaccineFinder.Models;
@@ -31,17 +38,52 @@ namespace VaccineFinder.Controllers
             IRestResponse response = client.Execute(request);
             var result = JsonConvert.DeserializeObject<LocationDistrict>(response.Content);
             return View(result);
+
         }
 
-        public string Showinfoloc(string dis, string dt)
+       
+
+        public ActionResult Showinfoloc(string dis, string dt)
         {
-            var client = new RestClient("https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=512&date=31-03-2021");
+            string datenow = DateTime.Now.ToString("dd-MM-yyyy");
+            string link = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=" + dis + "&date=" + datenow;
+            var client = new RestClient(link);
+            
             client.Timeout = -1;
             
             var request = new RestRequest(Method.GET);
             IRestResponse response = client.Execute(request);
-            var result = JsonConvert.DeserializeObject<ShowinfolocModel>(response.Content);
-            return "Success";
+
+            List<Sessions> result = new List<Sessions>();
+            var obj = JObject.Parse(response.Content);
+            foreach (var item in obj["sessions"])
+            {             
+                result.Add(new Sessions
+                {
+                    center_id = item["center_id"].ToString(),
+                    name = item["name"].ToString(),
+                    address = item["address"].ToString(),
+                    state_name = item["state_name"].ToString(),
+                    district_name = item["district_name"].ToString(),
+                    block_name = item["block_name"].ToString(),
+                    pincode = item["pincode"].ToString(),
+                    from = item["from"].ToString(),
+                    to = item["to"].ToString(),
+                    lat = item["lat"].ToString(),
+                    loclg = item["long"].ToString(),
+                    fee_type = item["fee_type"].ToString(),
+                    session_id = item["session_id"].ToString(),
+                    available_capacity_dose1 = item["available_capacity_dose1"].ToString(),
+                    available_capacity_dose2 = item["available_capacity_dose2"].ToString(),
+                    available_capacity = item["available_capacity"].ToString(),
+                    fee = item["fee"].ToString(),
+                    min_age_limit = item["min_age_limit"].ToString(),
+                    vaccine = item["vaccine"].ToString(),
+                    slots = item["slots"].ToString(),
+                }) ;
+            }
+            
+            return View(result);
         }
 
 
--- Models/Location.cs
+++ Models/Location.cs
@@ -8,27 +8,29 @@ namespace VaccineFinder.Models
     public class LocationState
     {
         public List<States> states { get; set; }
-        public string ttl1 { get; set; }
+        public string ttl1 { get; set; } = " ";
+
     }
     public class States 
     {
-        public string state_id { get; set; }
-        public string state_name { get; set; }
+        public string state_id { get; set; } = " ";
+        public string state_name { get; set; } = " ";
+        public string state_name_1 { get; set; } = " ";
     }
 
 
     public class LocationDistrict
     {
         public List<Districts> districts { get; set; }
-        public string ttl1 { get; set; }
+        public string ttl1 { get; set; } = " ";
     }
 
     public class Districts
     {
-        public string state_id { get; set; }
-        public string district_id { get; set; }
-        public string district_name { get; set; }
-        public string district_name_1 { get; set; }
+        public string state_id { get; set; } = " ";
+        public string district_id { get; set; } = " ";
+        public string district_name { get; set; } = " ";
+        public string district_name_1 { get; set; } = " ";
     }
 
 
@@ -39,33 +41,34 @@ namespace VaccineFinder.Models
     }
     public class Sessions
     {
-        public string center_id { get; set; }
-        public string name { get; set; }
-        public string name_1 { get; set; }
-        public string address { get; set; }
-        public string address_1 { get; set; }
-        public string state_name { get; set; }
-        public string state_name_l { get; set; }
-        public string district_name { get; set; }
+        public string center_id { get; set; } = " ";
+        public string name { get; set; } = " ";
+        //public string name_1 { get; set; } = " ";
+        public string address { get; set; } = " ";
+        //public string address_1 { get; set; } = " ";
+        public string state_name { get; set; } = " ";
+        //public string state_name_l { get; set; } = " ";
+        public string district_name { get; set; } = " ";
 
-        public string district_name_l { get; set; }
-        public string block_name { get; set; }
-        public string block_name_l { get; set; }
-        public string pincode { get; set; }
-        public string lat { get; set; }
-        public string longitude { get; set; }
-        public string from { get; set; }
-        public string to { get; set; }
-        public string fee_type { get; set; }
-        public string fee { get; set; }
-        public string session_id { get; set; }
-        public string date { get; set; }
-        public string available_capacity { get; set; }
-        public string available_capacity_dose1 { get; set; }
-        public string available_capacity_dose2 { get; set; }
-        public string min_age_limit { get; set; }
-        public string vaccine { get; set; }
-        public List<string> slots { get; set; }
+        //public string district_name_l { get; set; } = " ";
+        public string block_name { get; set; } = " ";
+        //public string block_name_l { get; set; } = " ";
+        public string pincode { get; set; } = " ";
+        public string lat { get; set; } = " ";
+        public string loclg { get; set; } = " ";
+        public string from { get; set; } = " ";
+        public string to { get; set; } = " ";
+        public string fee_type { get; set; } = " ";
+        public string fee { get; set; } = " ";
+        public string session_id { get; set; } = " ";
+        public string date { get; set; } = " ";
+        public string available_capacity { get; set; } = " ";
+        public string available_capacity_dose1 { get; set; } = " ";
+        public string available_capacity_dose2 { get; set; } = " ";
+        public string min_age_limit { get; set; } = " ";
+        public string vaccine { get; set; } = " ";
+        //public List<string> slots { get; set; }
+        public string slots { get; set; } = " ";
 
     }
 }
